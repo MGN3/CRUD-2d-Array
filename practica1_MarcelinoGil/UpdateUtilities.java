c5423cd7a7c3d92b031d618f6787c3df58f32c66
Marcelino Gil Nombela <112982522+MGN3@users.noreply.github.com>
2023-02-14 16:15:57 +0100

    Update UpdateUtilities.java

--- practica1_MarcelinoGil/UpdateUtilities.java
+++ practica1_MarcelinoGil/UpdateUtilities.java
@@ -43,18 +43,7 @@ public class UpdateUtilities {
 				.getString("Enter the value for column '" + (selectedCol + 1) + "'");
 	}
 
-	public static void deleteField(String[][] arrayString) {
-		int selectedRow;
-		int selectedCol;
-		// methor getIntBetween is in charge of setting the parameters, in this case,
-		// the valid rows to be modified go from 1 to String[][] length, both included.
-		selectedRow = Utilities.getIntBetween(1, arrayString.length, "Enter the row");
-		selectedRow -= 1;// -1 to match the array positions.
-		selectedCol = Utilities.getIntBetween(1, arrayString[selectedRow].length, "Enter the column/field.");
-		selectedCol -= 1;
-
-		arrayString[selectedRow][selectedCol] = null;
-	}
+	
 
 	/**
 	 * <ul>
